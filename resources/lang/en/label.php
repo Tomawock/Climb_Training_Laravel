9de9e60eb6426d59aedf8cccb19863601a7fab60
Tomawock <lori.tomasetti@hotmail.it>
2020-06-22 14:28:24 +0200

    inizilizzaione internazionalizzazione con introduzione pacchetto lingua italiana

---
+++ resources/lang/en/label.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'homePageTitle' => 'Home page',
+    'homePageNavbar' => 'Home',
+    'myTrainingNavbar' => 'My Training',
+    'accountNavbar' => 'Account',
+    'personalTrainingNavbar' => 'Personal Training Program',
+    'historyStatisticNavbar' => 'History and Statistics',
+    'trainingNavbar' => 'Training',
+    'exerciseListNavbar' => 'Exercise List',
+    'trainingListNavbar' => 'Training Program List',
+    
+    'welcome' => 'Welcome',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    
+    
+    
+];
--- storage/framework/views/aac94da58d5d9e2e00b7543e62da9322ed61fbc7.php
+++ storage/framework/views/aac94da58d5d9e2e00b7543e62da9322ed61fbc7.php
@@ -1,32 +1,34 @@
-<?php $__env->startSection('titolo', 'Home page'); ?>
+<?php $__env->startSection('titolo'); ?>
+<?php echo app('translator')->get('label.homePageTitle'); ?>
+<?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('stile', 'style.css'); ?>
 
 <?php $__env->startSection('left_navbar'); ?>
-<li><a href="<?php echo e(route('home')); ?>">Home</a></li>
+<li><a href="<?php echo e(route('home')); ?>"><?php echo app('translator')->get('label.homePageNavbar'); ?></a></li>
 <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Training<b class="caret"></b></a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo app('translator')->get('label.myTrainingNavbar'); ?><b class="caret"></b></a>
     <ul class="dropdown-menu">
-        <li><a href="<?php echo e(route('mytraining.information')); ?>">Account</a></li>
-        <li><a href="<?php echo e(route('mytraining.programlist')); ?>">Personal Training Program</a></li>
-        <li><a href="<?php echo e(route('mytraining.historystatistic')); ?>">History and Statistics</a></li>
+        <li><a href="<?php echo e(route('mytraining.information')); ?>"><?php echo app('translator')->get('label.accountNavbar'); ?></a></li>
+        <li><a href="<?php echo e(route('mytraining.programlist')); ?>"><?php echo app('translator')->get('label.personalTrainingNavbar'); ?></a></li>
+        <li><a href="<?php echo e(route('mytraining.historystatistic')); ?>"><?php echo app('translator')->get('label.historyStatisticNavbar'); ?></a></li>
     </ul>
 </li>
 <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Training<b class="caret"></b></a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo app('translator')->get('label.trainingNavbar'); ?><b class="caret"></b></a>
     <ul class="dropdown-menu">
-        <li><a href="<?php echo e(route('exercise.index')); ?>">Exercise List</a></li>
-        <li><a href="<?php echo e(route('trainingprogram.index')); ?>">Training Program List</a></li>
+        <li><a href="<?php echo e(route('exercise.index')); ?>"><?php echo app('translator')->get('label.exerciseListNavbar'); ?></a></li>
+        <li><a href="<?php echo e(route('trainingprogram.index')); ?>"><?php echo app('translator')->get('label.trainingListNavbar'); ?></a></li>
     </ul>
 </li>
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('right_navbar'); ?>
 <?php if($logged): ?>
-<li><a><i>Welcome <?php echo e($loggedName); ?></i></a></li>
-<li><a href="<?php echo e(route('user.logout')); ?>">logout <span class="glyphicon glyphicon-log-out"></span></a></li>
+<li><a><i><?php echo app('translator')->get('label.welcome'); ?> <?php echo e($loggedName); ?></i></a></li>
+<li><a href="<?php echo e(route('user.logout')); ?>"><?php echo app('translator')->get('label.logout'); ?> <span class="glyphicon glyphicon-log-out"></span></a></li>
 <?php else: ?>
-<li><a href="<?php echo e(route('user.login')); ?>"><span class="glyphicon glyphicon-user"></span> Log in</a></li>
+<li><a href="<?php echo e(route('user.login')); ?>"><span class="glyphicon glyphicon-user"></span> <?php echo app('translator')->get('label.login'); ?></a></li>
 <?php endif; ?>
 <?php $__env->stopSection(); ?>
 
