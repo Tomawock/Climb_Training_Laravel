41f344d50ac9bbc7773458b8c4e276992e8c23be
Lorenzo Tomasetti <lori.tomasetti@hotmail.it>
2020-06-29 12:03:00 +0200

    inserimento funzione per il download della scheda in pdf

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -1,144 +1,51 @@
-function checkAuthor(button)
-{
-    //alert(button.form.firstName.value + " " + button.form.lastName.value);
-    firstName = (button.form.firstName.value).trim();
-    firstName_msg = document.getElementById("invalid-firstName");
-    firstName_msg.innerHTML = "";
-
-
-    lastName = (button.form.lastName.value).trim();
-    lastName_msg = document.getElementById("invalid-lastName");
-    lastName_msg.innerHTML = "";
-
-
-    var regularExpression = new RegExp("^([a-zA-Z]+)$", "g");
-    var error = false;
-
-    if (firstName === "")
-    {
-        firstName_msg.innerHTML = "The firstName field must not be empty";
-        button.form.firstName.focus();
-        error = true;
-    } else if (!firstName.match(regularExpression))
-    {
-        firstName_msg.innerHTML = "The firstName must only contains letters, no digits or special characters";
-        button.form.firstName.focus();
-        error = true;
-    } else if (lastName === "")
-    {
-        lastName_msg.innerHTML = "The lastName field must not be empty";
-        button.form.lastName.focus();
-        error = true;
-    } else if (!lastName.match(regularExpression))
-    {
-        lastName_msg.innerHTML = "The lastName must only contains letters, no digits or special characters";
-        button.form.lastName.focus();
-        error = true;
-    }
-
-    if (!error)
-    {
-        if (button.value === "Create")
-        {
-            $.ajax({
-
-                type: 'GET',
-
-                url: '/ajax',
-
-                data: {firstName: firstName, lastName: lastName},
-
-                success: function (data) {
-
-                    if (data.found)
-                    {
-                        error = true;
-                        alert("Author already exists in the database");
-                    } else {
-                        button.form.submit();
-                    }
-                }
-
-            });
-        } else {
-            button.form.submit();
-        }
-    }
-}
-
-function checkBook(button)
-{
-    title = (button.form.title.value).trim();
-    title_msg = document.getElementById("invalid-title");
-    title_msg.innerHTML = "";
-    var error = false;
-
-    if (title === "")
-    {
-        title_msg.innerHTML = "The title field must not be empty";
-        button.form.title.focus();
-        error = true;
-    }
-
-    if (!error)
-    {
-        if (button.value === "Create")
-        {
-            $.ajax({
-
-                type: 'GET',
-
-                url: '/ajaxBook',
-
-                data: {title: title},
-
-                success: function (data) {
-
-                    if (data.found)
-                    {
-                        error = true;
-                        alert("Book title already exists in the database");
-                    } else {
-                        button.form.submit();
-                    }
-                }
-
-            });
-        } else {
-            button.form.submit();
-        }
-    }
-}
-
 function download() {
-
-//    var pdf = new jsPDF('l', 'pt', 'a4');
-//
-//
-//    var title = document.getElementById("title").innerHTML.trim();
-//
-//    pdf.autoTable({html: '#toDowload'})
-//    pdf.save('table.pdf')
-
+    
     var doc = new jsPDF('l', 'pt', 'a4');
 
     var pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
     var pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+
+    doc.setFontSize(20);
+    var title=document.getElementById("title").innerHTML.trim();
+    var lineHeight = doc.getLineHeight(title) / doc.internal.scaleFactor;
+    var splittedTextTitle = doc.splitTextToSize(title, pageWidth-40 );
+    var lines = splittedTextTitle.length  // splitted text; is a string array
+    var blockHeightTitle = lines * lineHeight;
     
     doc.setFontSize(20);
-    doc.text(document.getElementById("title").innerHTML.trim(), pageWidth/2, 20,'center');
+    var time =document.getElementById("time").innerHTML.trim()
+    var lineHeight = doc.getLineHeight(time) / doc.internal.scaleFactor;
+    var splittedTextTime = doc.splitTextToSize(time, pageWidth-40 );
+    var lines = splittedTextTime.length  // splitted text; is a string array
+    var blockHeightTimes = lines * lineHeight;
+    
     doc.setFontSize(10);
-    doc.text(document.getElementById("description").innerHTML.trim(), pageWidth/2, 40,'left');
-    doc.text(document.getElementById("time").innerHTML.trim(), pageWidth/2, 40,'center');
-
+    var desc = document.getElementById("description").innerHTML.trim();
+    var lineHeight = doc.getLineHeight(desc) / doc.internal.scaleFactor;
+    var splittedTextDesc = doc.splitTextToSize(desc, pageWidth-60 );
+    var lines = splittedTextDesc.length  // splitted text; is a string array
+    var blockHeightDesc = lines * lineHeight;
+    
+    var yPos = 30;
+    var xPos = pageWidth / 2;
+    doc.setFontSize(20);
+    doc.text(xPos, yPos, splittedTextTitle,'center');
+    yPos += blockHeightTitle;
+    doc.setFontSize(20);
+    doc.text(xPos, yPos, splittedTextTime,'center');
+    yPos += blockHeightTimes;
+    doc.setFontSize(10);
+    doc.text(xPos, yPos, splittedTextDesc,'center');
+    yPos += blockHeightDesc;
+    
     jsPDF.autoTableSetDefaults({
         headStyles: {fillColor: 0},
-        columnStyles: { halign: 'center'  }
+        columnStyles: {halign: 'center'},
     });
-    
+
     doc.autoTable({
         html: '#toDowload',
-        startY: 50
+        startY: yPos
     });
 
     doc.save('test_D.pdf');
