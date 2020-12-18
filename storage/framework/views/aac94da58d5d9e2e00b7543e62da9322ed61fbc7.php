16b75fc79a01ca706e4a20be029317ed3948fedb
Lorenzo Tomasetti <lori.tomasetti@hotmail.it>
2020-12-18 20:56:50 +0100

    update

--- storage/framework/views/aac94da58d5d9e2e00b7543e62da9322ed61fbc7.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('titolo_home'); ?>
-<?php echo app('translator')->get('label.homePageTitle'); ?>
-<?php $__env->stopSection(); ?>
-
-<?php $__env->startSection('stile', 'style.css'); ?>
-
-<?php echo $__env->make('layouts.home', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/tomawock/code/Climb_Training_Laravel/resources/views/index.blade.php ENDPATH**/ ?>
